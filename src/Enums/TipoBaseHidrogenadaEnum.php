b15443a89c538af5407b931335ce0f5e36eec4f5
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-04 21:18:16 -0300

    feat: add enum TipoBaseHidrogenadaEnum

---
+++ src/Enums/TipoBaseHidrogenadaEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wesley0010012\TradutorGenetico\Enums;
+
+enum TipoBaseHidrogenadaEnum: string
+{
+    case ADENINA = "A";
+    case TIMINA = "T";
+    case GUANINA = "G";
+    case URACILA = "U";
+    case CITOSINA = "C";
+}
