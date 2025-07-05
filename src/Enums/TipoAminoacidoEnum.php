8333fc528629861fd894ddafbe78a465586afe85
Wesley0010012 <wglaurindo33@gmail.com>
2025-07-04 21:22:53 -0300

    feat: add enum TipoAminoacidoEnum

---
+++ src/Enums/TipoAminoacidoEnum.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Wesley0010012\TradutorGenetico\Enums;
+
+enum TipoAminoacidoEnum: string
+{
+    case PHE = 'Phe';
+    case LEU = 'Leu';
+    case SER = 'Ser';
+    case TYR = 'Tyr';
+    case STOP = 'Stop';
+    case CYS = 'Cys';
+    case TRP = 'Trp';
+    case PRO = 'Pro';
+    case HIS = 'His';
+    case GLN = 'Gln';
+    case ARG = 'Arg';
+    case ILE = 'Ile';
+    case MET = 'Met';
+    case THR = 'Thr';
+    case ASN = 'Asn';
+    case LYS = 'Lys';
+    case VAL = 'Val';
+    case ALA = 'Ala';
+    case ASP = 'Asp';
+    case GLU = 'Glu';
+    case GLY = 'Gly';
+    case ND = "NÃO DEFINIDO";
+}
